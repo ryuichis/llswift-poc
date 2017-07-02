67f3ff14429ae78b9b933cd682925e73d2d4067e
Ryuichi Saito <ryuichi@ryuichisaito.com>
2017-07-02 12:26:33 -0500

    Generate executable binary

--- run.sh
+++ run.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-set -e
+# set -e
 
 make
 .build/debug/llswift $@
 
 echo "out.ll"
 cat out.ll
+
 echo "------------"
-echo "run"
+echo "lli run"
 clang+llvm-4.0.0-x86_64-apple-darwin/bin/lli out.ll
+
+echo "------------"
+echo "generate binary"
+clang+llvm-4.0.0-x86_64-apple-darwin/bin/clang out.ll
+
+echo "------------"
+echo "run a.out"
+./a.out
